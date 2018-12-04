613cf370fcb7fbfbb1b0057f441dbb6151234db0
Sean O Connor <s.m.oconnor@cit.ie>
2018-12-04 22:04:39 +0000

    first change for trigger

--- HelloPython.py
+++ HelloPython.py
@@ -2,7 +2,8 @@ import datetime
 print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
-print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
+print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+print ("first change for trigger")	
 
 
 	
